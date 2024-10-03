2a64ab9171e6d2fb906e7cfe253b96b2e180e69e
ggarcia453 <gg311137@gmail.com>
2024-10-03 14:40:51 -0700

    Setupscript update

--- setup.sh
+++ setup.sh
@@ -3,5 +3,13 @@
 sudo apt-get install -y -qq libasound2-dev libssl-dev libpulse-dev libdbus-1-dev portaudio19-dev librust-alsa-sys-dev
 echo "Please set up a defualt path for your music"
 read musicpath
+if [[ "${musicpath: -1}" != "\\" && "${musicpath: -1}" != "/" ]]; then
+    if [[ "$os_type" == "CYGWIN_NT" ]]; then
+        musicpath="${musicpath}\\\\"
+    else
+        musicpath="${musicpath}/"
+    fi
+fi
+
 echo "MUSICPATH=\"$musicpath\"" > .env
 cargo build
