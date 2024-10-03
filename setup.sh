58e44d3108eb66ce050918b31ad55ea29a452a1a
Gabriel Garcia <80603461+ggarcia453@users.noreply.github.com>
2024-10-03 14:52:11 -0700

    More shell script changes

--- setup.sh
+++ setup.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+read -p "Would you like to install the necessary packages? (y/n): " input
 
-sudo apt-get install -y -qq libasound2-dev libssl-dev libpulse-dev libdbus-1-dev portaudio19-dev librust-alsa-sys-dev
-echo "Please set up a defualt path for your music"
-read musicpath
+if [[ "$input" == "y" || "$input" == "Y" ]]; then
+    sudo apt-get install -y -qq libasound2-dev libssl-dev libpulse-dev libdbus-1-dev portaudio19-dev librust-alsa-sys-dev
+else
+    echo "Installation skipped."
+fi
+
+read  -p "Please set up a defualt path for your music: " musicpath
 if [[ "${musicpath: -1}" != "\\" && "${musicpath: -1}" != "/" ]]; then
-    if [[ "$os_type" == "CYGWIN_NT" ]]; then
+    if [[ "$os_type" == "CYGWIN_NT" || "$os_type" == "MINGW32_NT" || "$musicpath" == *"\\\\"* ]]; then
         musicpath="${musicpath}\\\\"
     else
         musicpath="${musicpath}/"
